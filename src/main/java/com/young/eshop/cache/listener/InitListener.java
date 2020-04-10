a3552f10bbfb8a2ae4bfee1087646780ddd787ab
Yangdaidai <1024826067@qq.com>
2020-04-10 11:50:10 +0800

    RedisConfiguration

--- src/main/java/com/young/eshop/cache/listener/InitListener.java
+++ src/main/java/com/young/eshop/cache/listener/InitListener.java
@@ -2,13 +2,9 @@ package com.young.eshop.cache.listener;
 
 import com.young.eshop.cache.rebuild.RebuildCacheThread;
 import com.young.eshop.cache.zookeeper.ZKDistributedLock;
-import com.young.eshop.cache.zookeeper.ZooKeeperSession;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -24,13 +20,14 @@ import javax.servlet.ServletContextListener;
  * @Version 1.0.0
  **/
 @Slf4j
+@Component
 public class InitListener implements ServletContextListener {
 
     public void contextInitialized(ServletContextEvent sce) {
 
-        new Thread(new RebuildCacheThread()).start();
         ZKDistributedLock.init();
         log.info("ZKDistributedLock init ...");
+        new Thread(new RebuildCacheThread()).start();
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
